0df97828cdaf3f8d8834acb6bd13cc6fc6dc15ad
Brian Kintz <bkintz@ele.uri.edu>
2012-03-14 22:40:35 -0400

    Modified to support more streamlined auto-updating.
    Updates to the UI List will now be handled by this class instead of by
    the individual vehicles.

--- src/edu/uri/ele/capstone/monitorfleet/DataFragment.java
+++ src/edu/uri/ele/capstone/monitorfleet/DataFragment.java
@@ -2,6 +2,9 @@ package edu.uri.ele.capstone.monitorfleet;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+
+import edu.uri.ele.capstone.monitorfleet.util.DataItem;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
@@ -15,6 +18,8 @@ import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 public class DataFragment extends ListFragment {
+	
+	private ArrayList<HashMap<String, String>> _listData = new ArrayList<HashMap<String, String>>();
 	
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
@@ -31,6 +36,11 @@ public class DataFragment extends ListFragment {
 	public void onViewCreated(View view, Bundle savedInstanceState){
 		final ListView lv = getListView();
 
+		setListAdapter(new SimpleAdapter(getActivity(), _listData, R.layout.list_item,
+	  			 new String[] { "displayName", "value" },
+	  			 new int[] { R.id.item_title, R.id.item_subtitle })
+		);
+		
 		lv.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener(){
 			public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
 				AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;		
@@ -39,7 +49,6 @@ public class DataFragment extends ListFragment {
 				menu.add("Machine Name: " + item.get("machineName"));
 				menu.add("Value: " + item.get("value"));
 			}
-			
 		});
 		
 		lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -54,9 +63,44 @@ public class DataFragment extends ListFragment {
 		getActivity().findViewById(R.id.list_nodata).setVisibility(enable ? View.INVISIBLE : View.VISIBLE);
 	}
 	
-	public void updateListContent(ArrayList<HashMap<String, String>> newData){
-		setListAdapter(new SimpleAdapter(getActivity(), newData, R.layout.list_item,
-	  			 new String[] { "displayName", "value" },
-	  			 new int[] { R.id.item_title, R.id.item_subtitle }));
+	public void updateListContent(List<DataItem> newData, boolean clear){
+
+		if (clear) _listData.clear();
+		
+		if(_listData.size() == 0){
+			for(int i = 0; i < newData.size(); i++){
+				DataItem d = (DataItem)newData.get(i);
+	
+				_listData.add(d.getStringHashMap());
+			}
+		}else{
+			int j_st = 0;
+			
+			for(int i = 0; i < newData.size(); i++){
+				if(j_st >= _listData.size()){
+					_listData.add(((DataItem)newData.get(i)).getStringHashMap());
+					continue;
+				}
+				
+				for(int j = j_st; j < _listData.size(); j++){
+					DataItem cur_di = (DataItem)newData.get(i);
+					HashMap<String, String> cur_hm = _listData.get(j);
+					
+					int cmp = cur_di.getMachineName().compareTo(cur_hm.get("machineName"));
+					
+					if(cmp == 0){ // found -> update
+						cur_hm.put("value", cur_di.getValue());
+						j_st = j + 1;
+						break;
+					}else if(cmp > 0){  // missing -> insert
+						_listData.add(j, cur_di.getStringHashMap());
+						j_st = j + 1;
+						break;
+					}
+				}
+			}
+		}
+		
+		((SimpleAdapter)getListAdapter()).notifyDataSetChanged();
 	}
 }
